cecf9b8c0aa4de439b97a350d969174f57dc6a5e
RustamovRRR <rustamrrustamov@gmail.com>
2022-05-11 21:43:31 +0400

    FloatingActionButton widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.teal,
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: Colors.red)),
       home: Scaffold(
         appBar: AppBar(
           title: Text("First App"),
-          backgroundColor: Colors.lightGreen,
         ),
-        body: Text("Hello World", style: TextStyle(fontSize: 24)),
+        body: Text("Hello World",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.account_balance_sharp),
+        ),
       ),
     );
   }
