a4eab23379bb21461792a6a11d5eedd0cc3c331e
RustamovRRR <rustamrrustamov@gmail.com>
2022-05-16 22:43:35 +0400

    understanding row

--- lib/main.dart
+++ lib/main.dart
@@ -18,38 +18,82 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("First App"),
         ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              'Germany',
-              style: TextStyle(fontSize: 36),
-            ),
-            decoration: BoxDecoration(
-                color: Colors.amberAccent,
-                shape: BoxShape.rectangle,
-                border: Border.all(
-                    color: Colors.brown, style: BorderStyle.solid, width: 4),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    bottomRight: Radius.circular(20)),
-                image: DecorationImage(
-                  image: NetworkImage(_img),
-                  fit: BoxFit.cover,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(20, 30),
-                    blurRadius: 10,
-                  )
-                ]),
+        body: Container(
+          color: Colors.teal.shade100,
+          height: 400,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.green,
+              ),
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.orange,
+              ),
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.red,
+              ),
+              Icon(
+                Icons.add_circle,
+                size: 64,
+                color: Colors.yellow,
+              ),
+            ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Icon(Icons.account_balance_sharp),
         ),
+      ),
+    );
+  }
+}
+
+class ContainerDersleri extends StatelessWidget {
+  const ContainerDersleri({
+    Key? key,
+    required String img,
+  })  : _img = img,
+        super(key: key);
+
+  final String _img;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Text(
+          'Germany',
+          style: TextStyle(fontSize: 36),
+        ),
+        decoration: BoxDecoration(
+            color: Colors.amberAccent,
+            shape: BoxShape.rectangle,
+            border: Border.all(
+                color: Colors.brown, style: BorderStyle.solid, width: 4),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+            image: DecorationImage(
+              image: NetworkImage(_img),
+              fit: BoxFit.cover,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(20, 30),
+                blurRadius: 10,
+              )
+            ]),
       ),
     );
   }
